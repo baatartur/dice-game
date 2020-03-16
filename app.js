8ea60472e8963a8657fef4d4f645e84c216a01df
toroo <davaajavbaatartur@gmail.com>
2020-03-17 01:07:16 +0800

    porject kod ehlel

---
+++ app.js
@@ -0,0 +1,17 @@
+// Тоглогчийн ээлжийг хадгалах хувьсагч
+var activePlayer = 1;
+var activePlayer = 0;
+
+// Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
+var scores = [0, 0];
+// Тоглогчийн ээлжинд цуглуулж байгаа оноог хадгалах хувьсагч
+var roundScores = 0;
+
+//Шоо буулгахад санамсаргүй 1-6 хүртэл буулгах хувьсагч
+var dice = Math.floor(Math.random() * 6) + 1;
+document.querySelector("#score-0").textContent = 0;
+document.querySelector("#score-1").textContent = 0;
+document.querySelector("#current-0").textContent = 0;
+document.querySelector("#current-1").textContent = 0;
+document.querySelector(".dice").style.display = "none";
+console.log(" Шоо " + dice);
