77fdf9f8334027888789bb1294ec9779e2fa08af
toroo <davaajavbaatartur@gmail.com>
2020-03-17 23:55:14 +0800

    shoog shiddeg bolgson

--- app.js
+++ app.js
@@ -9,9 +9,14 @@ var roundScores = 0;
 
 //Шоо буулгахад санамсаргүй 1-6 хүртэл буулгах хувьсагч
 var dice = Math.floor(Math.random() * 6) + 1;
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
-console.log(" Шоо " + dice);
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+
+var diceDom = document.querySelector(".dice");
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var dice = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + dice + ".png";
+});
