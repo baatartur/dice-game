9628ac5b48be56cb67c58bd03be0bf9845c41621
baatartur <62394656+baatartur@users.noreply.github.com>
2020-03-22 22:29:23 +0800

    app.js kodiig oorchilson

--- app.js
+++ app.js
@@ -1,37 +1,58 @@
-// Тоглогчийн ээлжийг хадгалах хувьсагч
+//Шоо буулгахад санамсаргүй 1-6 хүртэл буулгах хувьсагч
+newGame();
+var isNewGame;
 var activePlayer;
+
 // Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
 var scores;
 // Тоглогчийн ээлжинд цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScores;
-//Шоо буулгахад санамсаргүй 1-6 хүртэл буулгах хувьсагч
-newGame();
+// Тоглоомын төгөсгөл
+
 // шоо зургаар буудаг хувьсагч
 var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 document.querySelector(".btn-roll").addEventListener("click", function() {
-  var dice = Math.floor(Math.random() * 7);
-  diceDom.style.display = "block";
-  diceDom.src = "dice-" + dice + ".png";
-  // Тоглогчийн ээлжийн оноог өөрчилнөө
-  if (dice !== 0) {
-    roundScores = roundScores + dice;
-    document.getElementById(
-      "current-" + activePlayer
-    ).textContent = roundScores;
+  if (isNewGame) {
+    var dice = Math.floor(Math.random() * 7);
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + dice + ".png";
+    // Тоглогчийн ээлжийн оноог өөрчилнөө
+    if (dice !== 0) {
+      roundScores = roundScores + dice;
+      document.getElementById(
+        "current-" + activePlayer
+      ).textContent = roundScores;
+    } else {
+      nextPlayer();
+    }
   } else {
-    nextPlayer();
+    alert("Тоглоом дууссан тул New Game товчийг дарнуу");
   }
 });
+
 // Hold list эвэнт хийх
 document.querySelector(".btn-hold").addEventListener("click", function() {
-  scores[activePlayer] = scores[activePlayer] + roundScores;
-  document.getElementById("score-" + activePlayer).textContent =
-    scores[activePlayer];
-  if (scores[activePlayer] >= 100) {
-    document.getElementById("name-" + activePlayer).textContent =
-      " Танд баяр хүргье !!! ";
+  if (isNewGame) {
+    scores[activePlayer] = scores[activePlayer] + roundScores;
+    document.getElementById("score-" + activePlayer).textContent =
+      scores[activePlayer];
+    // ssss
+    if (scores[activePlayer] >= 100) {
+      isNewGame = false;
+      document.getElementById("name-" + activePlayer).textContent =
+        " Та хожилоо ";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+    } else {
+      nextPlayer();
+    }
   } else {
-    nextPlayer();
+    alert("Тоглоом дууссан тул New Game товчийг дарнуу");
   }
 });
 function nextPlayer() {
@@ -46,15 +67,21 @@ document.querySelector(".btn-new").addEventListener("click", function() {
   newGame();
 });
 function newGame() {
+  isNewGame = true;
   activePlayer = 0;
   // Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
   scores = [0, 0];
   // Тоглогчийн ээлжинд цуглуулж байгаа оноог хадгалах хувьсагч
   roundScores = 0;
   //Шоо буулгахад санамсаргүй 1-6 хүртэл буулгах хувьсагч
+
   var dice = Math.floor(Math.random() * 7);
   document.getElementById("score-0").textContent = 0;
   document.getElementById("score-1").textContent = 0;
   document.getElementById("current-0").textContent = 0;
   document.getElementById("current-1").textContent = 0;
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
 }
